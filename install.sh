cca777a5d6194f248ece6d74b2a64b8f0ac20cf6
Aleksander Smywinski-Pohl <apohllo@o2.pl>
2015-10-01 19:28:48 +0200

    Installation script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+echo 'Installing Vim configuration'
+if [ -e ~/.vim ]
+then
+	mv ~/.vim ~/.vim.old
+fi
+if [ -e ~/.vimrc ]
+then
+	mv ~/.vimrc ~/.vimrc.old
+fi
+ln -s `pwd`/vim ~/.vim
+ln -s `pwd`/vimrc ~/.vimrc
+
+echo 'Installing Tmux configuration'
+if [ -e ~/.tmux.conf ]
+then
+	mv ~/.tmux.conf ~/.tmux.conf.old
+fi
+ln -s `pwd`/tmux.conf ~/.tmux.conf
